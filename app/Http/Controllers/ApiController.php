dfa3e30c62aa42b0186aa7ad92b7137ec02cfdc4
Jeidsan A. da C. Pereira <jeidsan.pereira@gmail.com>
2026-03-15 11:51:10 -0300

    Criando endpoint para listagem de clientes

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Client;
+use Illuminate\Http\Request;
+
+class ApiController extends Controller
+{
+    public function status()
+    {
+        return response()->json(
+            [
+                'status' => 'OK',
+                'message' => 'API is running'
+            ],
+            200
+        );
+    }
+
+    public function clients()
+    {
+        $clients = Client::all();
+
+        return response()->json(
+            [
+                'status' => 'OK',
+                'message' => 'Success',
+                'data' => $clients
+            ],
+            200
+        );
+    }
+}
